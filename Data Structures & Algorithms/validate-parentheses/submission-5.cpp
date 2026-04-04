62785ab76cdb15b14b1419092ac16294b15aefc2
Parth Nimkar <parthnimkar01@gmail.com>
2026-04-04 13:59:48 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidPair(char open,char close){
+        return (open == '(' && close == ')') || 
+               (open == '[' && close == ']') || 
+               (open == '{' && close == '}');
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch : s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                if(!isValidPair(st.top(),ch)){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
